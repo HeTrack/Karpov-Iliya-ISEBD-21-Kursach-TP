d70f5516063ec4dc9536c868df8c0096ab1303f6
iliya.utd2014 <iliya.utd2014@gmail.com>
2020-06-12 11:17:06 +0400

    fix courses

--- UniversityYouExpelled/UniversityWebClient/Controllers/CourseController.cs
+++ UniversityYouExpelled/UniversityWebClient/Controllers/CourseController.cs
@@ -3,22 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using UniversityBusinessLogic.BindingModels;
 using UniversityBusinessLogic.Interfaces;
-using UniversityDataBaseImplement.Implements;
+using UniversityBusinessLogic.ViewModels;
+using UniversityWebClient.Models;
 
 namespace UniversityWebClient.Controllers
 {
     public class CourseController : Controller
-    {
-        private readonly ICourseLogic course;
-        public CourseController(CourseLogic course)
-        {
-           this.course = course;
-        }
-        public IActionResult Course()
-        {
-            ViewBag.Courses = course.Read(null);
-            return View();
+    {   
+            private readonly ICourseLogic _course;
+            public CourseController(ICourseLogic course)
+            {
+                _course = course;
+            }
+            public IActionResult Course()
+            {
+                ViewBag.Courses = _course.Read(null);
+                return View();
+            }
         }
     }
-}
